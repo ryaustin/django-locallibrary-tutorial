7395ff43039f9cf47b937f32b6165218a988c3d2
ryaustin <rrarra@gmail.com>
2022-03-01 10:44:15 -0500

    updated paths for both book stores

--- catalog/urls.py
+++ catalog/urls.py
@@ -42,10 +42,20 @@ urlpatterns += [
 # Add URLConf for book store
 urlpatterns += [
     path('store/', views.book_store, name='store'),
-    path('store/add/<book_id>', views.add_to_cart, name='add-to-cart'),
-    path('store/add/<book_id>/<go_to_cart>', views.add_to_cart, name='add-to-cart'),
-    path('store/remove/<book_id>', views.remove_from_cart, name='remove-from-cart'),
-    path('store/clear_cart', views.clear_cart, name='clear-cart'),
+    path('store/<use_htmx>', views.book_store, name='store'),
+    path('store/book/add/<book_id>', views.add_to_cart, name='add-to-cart'),
+    path('store/book/add/<book_id>/<go_to_cart>', views.add_to_cart, name='add-to-cart'),
+    path('store/book/add/<book_id>/<go_to_cart>/<use_htmx>', views.add_to_cart, name='add-to-cart'),
+    path('store/book/remove/<book_id>', views.remove_from_cart, name='remove-from-cart'),
+    path('store/book/remove/<book_id>/<use_htmx>', views.remove_from_cart, name='remove-from-cart'),
+    path('store/clear_cart/', views.clear_cart, name='clear-cart'),
+    path('store/clear_cart/<use_htmx>', views.clear_cart, name='clear-cart'),
     path('store/cart_detail/<cart_id>', views.cart_detail, name='cart-detail'),
+    path('store/cart_detail/<cart_id>/<use_htmx>', views.cart_detail, name='cart-detail'),
     
+    # Patterns for partials
+    path('store/book_details/<book_id>', views.book_detail_partial, name='book-detail-partial'),
+    path('store/cart_button_partial/<cart_id>', views.cart_button_info_partial, name='cart-button-partial'),
+    path('store/cart_total_row_partial/<cart_id>', views.cart_total_row_partial, name='cart-total-row'),
+
 ]
