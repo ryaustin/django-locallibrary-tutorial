a729796ebaab26dbed827531ebd86de8de53c21e
ry_austin <rrarra@gmail.com>
2022-11-07 22:24:43 -0500

    stripped away the code and added comments to rebuild live.

--- catalog/integrations/zoho_books/zoho_views.py
+++ catalog/integrations/zoho_books/zoho_views.py
@@ -1,98 +1,78 @@
 '''
 Views specific to Zoho Books Integration
-documentation: https://www.zoho.com/books/api/v3/
+
+The OAuth dance is as follows:
+    1. Redirect to Zoho Books OAuth page
+    2. Zoho Books redirects back to our callback URL with a code
+    3. Use the code to get an access token
+    4. Store the access token in the database
+
+Documentation: https://www.zoho.com/books/api/v3/
 '''
 
-import requests
+# place imports at the top of the file
+import requests # the requests library is used to make HTTP requests
+# httpresponse is a django shortcut that returns an http response
+# redirect is a django shortcut that redirects to a url
 from django.shortcuts import HttpResponse, redirect
-from django.views.decorators.csrf import csrf_exempt
+# login_required is a django shortcut that redirects to the login page if the user is not logged in
 from django.contrib.auth.decorators import login_required
+# messages is a django shortcut that allows us to display messages to the user
 from django.contrib import messages
+# config from the decouple library is used to get environment variables from the .env file
 from decouple import config
-import secrets
 
+# Create your views here.
+# first view should be the one that redirects to the oauth page connect_to_zoho_books?
 @login_required
 def connect_to_zoho_books(request):
     """
-    Connect to Zoho Books API
+    Connect to Zoho Books API. Your app must be registered with Zoho Books to use this.
+    Then follow the documentation to build the request_url and redirect the user to it. 
     """
-    # session = request.session 
+    # 1. Build the request_uri
+    # 2. Redirect the user to the request_uri
+    # TODO: Implement this
+    pass
 
-    # store app state in session
-    # session['app_state'] = secrets.token_urlsafe(16) # generate random string
-    scope = "ZohoBooks.fullaccess.all"
-    ZOHO_API_CLIENT_ID = config("ZOHO_API_CLIENT_ID", "")
-    # state = session['app_state']
-    # we can then use this state to verify the response
-    # from Zoho Books by storing it in the db and comparing it to the state
-    # returned by Zoho Books. For testing purposes, we'll just use a simple string.
-    state = 'testing'
-    response_type = "code"
-    ZOHO_API_REDIRECT_URI = config(
-        "ZOHO_API_REDIRECT_URI",
-        "",
-    )
-    access_type = "offline"
-    authorization_url = "https://accounts.zoho.com/oauth/v2/auth?"
-    request_params = f"scope={scope}&client_id={ZOHO_API_CLIENT_ID}&state={state}&response_type={response_type}&redirect_uri={ZOHO_API_REDIRECT_URI}&access_type={access_type}"
-    request_uri = authorization_url + request_params
-    print(request_uri)
-    print(state)
-    return redirect(request_uri)
 
-@csrf_exempt
 @login_required
 def zoho_books_callback(request):
     """
     Callback for Zoho Books API
     """
-    state = request.GET.get("state")
-    if not state == 'testing':
-        return HttpResponse("Invalid state")
-    else:
-        try:
-            code = request.GET["code"]
-        except KeyError:
-            code = None
-        if not code:
-            messages.error(
-                request,
-                f"There was an error connecting to zoho books. Please contact your Zoho admin if the problem persists.",
-            )
-            return redirect("integrations")
-    query_params = {
-                    'code': code,
-                    'client_id': config("ZOHO_API_CLIENT_ID", ""),
-                    'client_secret': config("ZOHO_API_CLIENT_SECRET", ""),
-                    'redirect_uri': config("ZOHO_API_REDIRECT_URI", ""),
-                    'grant_type': 'authorization_code',
-                    }
-    response = requests.post('https://accounts.zoho.com/oauth/v2/token', data=query_params)
-    print(response.json())
-    if response.status_code == 200:
-        return store_token(request, response, service_name='zoho_books')
-    else:
-        error_message = (
-            response.json().get("error", {}).get("error_text", "Unknown Error")
-        )
-        raise KeyError(f"HTTP.Error.{response.status_code}: {error_message}")
-
+    # 1. Check the state parameter to make sure it matches the one you sent
+    # 2. Get the code from the request
+    # 3. Build the query parameters for the POST request based on the documentation
+    # 4. Make the POST request to get the access token
+    # 5. Store the access token in the database
+    # 6. Redirect the user to the integrations page
+    # TODO: Implement this
+    
+    pass
+    
+  
 def store_token(request, response, service_name: str):
     """
     Store token in database
     """
-    profile = request.user.profile
-    profile.integration_info[service_name] = response.json()
-    profile.save()
-    messages.success(request, "Successfully connected to Zoho Books")
-    return redirect("integrations")
+    # 1. Get the access token from the response
+    # 2. Store the access token in the database under the user's profile
+    # in the integration_info field
+    # 3. Redirect the user to the integrations page
+    # 4. Display a success message to the user
+    # TODO: Implement this
+    
+    pass
+   
 
 def disconnect_from_zoho_books(request):
     """
     Disconnect from Zoho Books API
     """
-    profile = request.user.profile
-    profile.integration_info['zoho_books'] = {}
-    profile.save()
-    messages.success(request, "Successfully disconnected from Zoho Books")
-    return redirect("integrations")
+    # 1. Delete the access token from the database
+    # 2. Redirect the user to the integrations page
+    # 3. Display a success message to the user
+    # TODO: Implement this
+    
+    pass
